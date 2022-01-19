6ce62b59c680ca1e7d897496485fd6a214224697
andre <andrelaprov@gmail.com>
2022-01-19 18:48:14 -0500

    Updates

---
+++ precache-manifest.d283a5ff0c851b9df6a634a2e33f1089.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0aedbb54e848237aa06701dfd78284",
+    "url": "/no-db-project-baby-registry/index.html"
+  },
+  {
+    "revision": "3d8a118bdb90672e95a0",
+    "url": "/no-db-project-baby-registry/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "96e26de27a07f1131dcc",
+    "url": "/no-db-project-baby-registry/static/css/main.fc280db2.chunk.css"
+  },
+  {
+    "revision": "3d8a118bdb90672e95a0",
+    "url": "/no-db-project-baby-registry/static/js/2.ca04b5bd.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/no-db-project-baby-registry/static/js/2.ca04b5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e26de27a07f1131dcc",
+    "url": "/no-db-project-baby-registry/static/js/main.c4203a45.chunk.js"
+  },
+  {
+    "revision": "292f64c25bd3684b0ad5",
+    "url": "/no-db-project-baby-registry/static/js/runtime-main.882e612b.js"
+  },
+  {
+    "revision": "a1acc7340921dfe593cbf3c3bd1ff76b",
+    "url": "/no-db-project-baby-registry/static/media/editOne.a1acc734.png"
+  },
+  {
+    "revision": "b36173c1bf51a26bce11ed454ec24333",
+    "url": "/no-db-project-baby-registry/static/media/purplePin.b36173c1.png"
+  }
+]);
