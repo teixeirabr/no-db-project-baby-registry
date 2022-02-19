2e8e8d7582057371a349181d7ad5feeec4c8d0b2
Andre <andre@A-MacBook-Air.local>
2022-02-19 00:10:54 -0500

    server controller

--- server/controller.js
+++ server/controller.js
@@ -1,27 +1,26 @@
 const babyGift = [
   {
     id: 0,
-    name: "Andre Teixeira",
+    name: "John Smith ",
     email: "andrenone@gmail.com",
-    gifts: ["Rocking Chair", "Dresser"]
+    gifts: ["Rocking Chair", "Dresser"],
   },
   {
     id: 1,
-    name: "Flavia Teixeira",
+    name: "Mary Steven",
     email: "flavianone@gmail.com",
-    gifts: ["Baby Monitor", "Lamp"]
+    gifts: ["Baby Monitor", "Lamp"],
   },
   {
     id: 2,
-    name: "Carla Teixeira",
+    name: "Peter Jones",
     email: "flavia@gmail.com",
-    gifts: ["Bottle", "Sweater"]
-  }
+    gifts: ["Bottle", "Sweater"],
+  },
 ];
 let id = 3;
 
 module.exports = {
-  
   getGifts: (req, res) => {
     res.status(200).send(babyGift);
   },
@@ -34,7 +33,7 @@ module.exports = {
 
   deleteGifts: (req, res) => {
     const { newId } = req.params;
-    
+
     console.log(req.params);
     const index = babyGift.findIndex((gift, id) => {
       console.log("index: ", id, " -> ", gift.id, +newId);
@@ -44,24 +43,24 @@ module.exports = {
     babyGift.splice(index, 1);
     res.status(200).send(babyGift);
   },
-  
+
   editGifts: (req, res) => {
     const { newId } = req.params;
     console.log(newId);
     const { name, email, gifts } = req.body;
     console.log(req.body);
-    const index = babyGift.findIndex(e => e.id === +newId);
+    const index = babyGift.findIndex((e) => e.id === +newId);
     console.log(index);
 
     const newGift = {
       id: +newId || babyGift[index].id,
       name: name || babyGift[index].name,
       email: email || babyGift[index].email,
-      gifts: gifts || babyGift[index].gifts
+      gifts: gifts || babyGift[index].gifts,
     };
     console.log(newGift);
     babyGift.splice(index, 1, newGift);
     // console.log(babyGift);
     res.status(200).send(babyGift);
-  }
+  },
 };
